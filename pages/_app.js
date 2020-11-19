212a7a68fd374a461dbdb9adf986f7df42dec798
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-19 22:41:12 +1000

    Integrated express server

--- pages/_app.js
+++ pages/_app.js
@@ -11,28 +11,38 @@ import Hero from "../components/shared/Hero";
 // all route request will go through this file
 function MyApp({ Component, pageProps }) {
   // console.log(Component); // show requested component in backend terminal
+
+  const isHomePage = () => Component.name === "Home";
+
   return (
     <div className="portfolio-app">
       <NavBar />
-      {pageProps.appData}
-      {Component.name === "Home" && <Hero />}
+      {isHomePage() && <Hero />}
       <div className="container">
         <Component {...pageProps} />
       </div>
+      {isHomePage() && (
+        <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
+          <div className="container text-center">
+            <small>Copyright &copy; Your Website</small>
+          </div>
+        </footer>
+      )}
     </div>
   );
 }
 
-// if getInitialProps is added in _app.js, "static optimisation" is disabled for all pages
-MyApp.getInitialProps = async (context) => {
-  console.log("GET INITIAL PROPS _APP");
-  // App.getInitialProps() is an async func
-  const initialProps =
-    App.getInitialProps && (await App.getInitialProps(context));
-  console.log(initialProps); // { pageProps: { (initial props of each page) } }
-  return {
-    pageProps: { appData: "Hello _App Component", ...initialProps.pageProps },
-  };
-};
+// // getInitialProps is called on the server
+// // if getInitialProps is added in _app.js, "static optimisation" is disabled for all pages
+// MyApp.getInitialProps = async (context) => {
+//   console.log("GET INITIAL PROPS _APP");
+//   // App.getInitialProps() is an async func
+//   const initialProps =
+//     App.getInitialProps && (await App.getInitialProps(context));
+//   // console.log(initialProps); // { pageProps: { (initial props of each page) } }
+//   return {
+//     pageProps: { appData: "Hello _App Component", ...initialProps.pageProps },
+//   };
+// };
 
 export default MyApp;
