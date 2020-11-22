5c258ee068febdeb5d86662bc3034d4dbbba2725
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-22 13:25:19 +1000

    Implement GraphQL in Express

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const next = require("next");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -9,6 +11,31 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
+  // construct a schema, using GRAPHQL schema language
+  const schema = buildSchema(`
+    type Query {
+      hello: String
+    }
+  `);
+
+  // root provides a resolver for each API endpoiint
+  const root = {
+    hello: () => {
+      return "Hello World!";
+    },
+  };
+
+  // GraphQL routes
+  server.use(
+    "/graphql",
+    graphqlHTTP({
+      schema,
+      rootValue: root,
+      graphiql: true,
+    })
+  );
+
+  // route all other requests to next.js handler
   server.all("*", (req, res) => {
     return handle(req, res);
   });
