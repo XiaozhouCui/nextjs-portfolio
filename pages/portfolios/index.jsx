adc1980ad83e62c1a0d310825761c3fb3f49720f
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-24 11:09:35 +1000

    Rendered portfolio cards from GraphQL data

--- pages/portfolios/index.jsx
+++ pages/portfolios/index.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import PortfolioCard from "@/components/portfolios/PortfolioCard";
 
 const Portfolios = ({ portfolios }) => {
   return (
@@ -10,54 +11,13 @@ const Portfolios = ({ portfolios }) => {
           </div>
         </div>
       </section>
-      {JSON.stringify(portfolios)}
       <section className="pb-5">
         <div className="row">
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
+          {portfolios.map((portfolio) => (
+            <div key={portfolio._id} className="col-md-4">
+              <PortfolioCard portfolio={portfolio} />
             </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2 ">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2 ">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
     </>
@@ -85,6 +45,8 @@ const fetchPortfolios = () => {
         location
         jobTitle
         description
+        startDate
+        endDate
       }
     }`;
   // GQL uses POST request, passing in query as payload
