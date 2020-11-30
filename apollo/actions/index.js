88c938a701a27e32ed7a81c89ba4677392691321
Joe Cui <xiaozhou.cui@gmail.com>
2020-12-01 08:56:21 +1000

    Refactor: extracted Apollo actions

---
+++ apollo/actions/index.js
@@ -0,0 +1,39 @@
+import { useLazyQuery, useQuery, useMutation } from "@apollo/client";
+import {
+  GET_PORTFOLIOS,
+  CREATE_PORTFOLIO,
+  UPDATE_PORTFOLIO,
+  DELETE_PORTFOLIO,
+} from "@/apollo/queries";
+
+// Apollo Client hooks
+// const [getPortfolios, { loading, data }] = useLazyQuery(GET_PORTFOLIOS); // lazyquery to be used with useEffect.
+export const useGetPortfolio = () => useQuery(GET_PORTFOLIOS);
+
+// no need to pass in portfolio ID here, apollo can figure it out
+export const useUpdatePortfolio = () => useMutation(UPDATE_PORTFOLIO);
+
+export const useDeletePortfolio = () =>
+  useMutation(DELETE_PORTFOLIO, {
+    update(cache, { data: { deletePortfolio } }) {
+      const { portfolios } = cache.readQuery({ query: GET_PORTFOLIOS });
+      const newPortfolios = portfolios.filter((p) => p._id !== deletePortfolio);
+      cache.writeQuery({
+        query: GET_PORTFOLIOS,
+        data: { portfolios: newPortfolios },
+      });
+    },
+  });
+
+export const useCreatePortfolio = () =>
+  useMutation(CREATE_PORTFOLIO, {
+    update(cache, { data: { createPortfolio } }) {
+      // get old portfolios from cache
+      const { portfolios } = cache.readQuery({ query: GET_PORTFOLIOS });
+      // add newly created portfolio into cache
+      cache.writeQuery({
+        query: GET_PORTFOLIOS, // re-run getPortfolios query
+        data: { portfolios: [...portfolios, createPortfolio] },
+      });
+    },
+  });
--- pages/portfolios/index.jsx
+++ pages/portfolios/index.jsx
@@ -1,45 +1,19 @@
 import PortfolioCard from "@/components/portfolios/PortfolioCard";
 import Link from "next/link";
-import { useLazyQuery, useQuery, useMutation } from "@apollo/client";
 import {
-  GET_PORTFOLIOS,
-  CREATE_PORTFOLIO,
-  UPDATE_PORTFOLIO,
-  DELETE_PORTFOLIO,
-} from "@/apollo/queries";
+  useGetPortfolio,
+  useUpdatePortfolio,
+  useDeletePortfolio,
+  useCreatePortfolio,
+} from "@/apollo/actions";
 import withApollo from "@/hoc/withApollo";
 import { getDataFromTree } from "@apollo/react-ssr";
 
 const Portfolios = () => {
-  // Apollo Client hooks
-  // const [getPortfolios, { loading, data }] = useLazyQuery(GET_PORTFOLIOS); // lazyquery to be used with useEffect.
-  const { data } = useQuery(GET_PORTFOLIOS);
-
-  // no need to pass in portfolio ID here, apollo can figure it out
-  const [updatePortfolio] = useMutation(UPDATE_PORTFOLIO);
-
-  const [deletePortfolio] = useMutation(DELETE_PORTFOLIO, {
-    update(cache, { data: { deletePortfolio } }) {
-      const { portfolios } = cache.readQuery({ query: GET_PORTFOLIOS });
-      const newPortfolios = portfolios.filter((p) => p._id !== deletePortfolio);
-      cache.writeQuery({
-        query: GET_PORTFOLIOS,
-        data: { portfolios: newPortfolios },
-      });
-    },
-  });
-
-  const [createPortfolio] = useMutation(CREATE_PORTFOLIO, {
-    update(cache, { data: { createPortfolio } }) {
-      // get old portfolios from cache
-      const { portfolios } = cache.readQuery({ query: GET_PORTFOLIOS });
-      // add newly created portfolio into cache
-      cache.writeQuery({
-        query: GET_PORTFOLIOS, // re-run getPortfolios query
-        data: { portfolios: [...portfolios, createPortfolio] },
-      });
-    },
-  });
+  const { data } = useGetPortfolio();
+  const [updatePortfolio] = useUpdatePortfolio();
+  const [deletePortfolio] = useDeletePortfolio();
+  const [createPortfolio] = useCreatePortfolio();
 
   const portfolios = (data && data.portfolios) || [];
 
