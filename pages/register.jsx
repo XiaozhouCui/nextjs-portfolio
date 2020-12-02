4812f1964891fc7b80b824734248fdd2f5eb8477
Joe Cui <xiaozhou.cui@gmail.com>
2020-12-02 17:55:41 +1000

    Frontend: added Register Form page

--- pages/register.jsx
+++ pages/register.jsx
@@ -1,9 +1,21 @@
-const register = () => {
+import RegisterForm from "@/components/forms/RegisterForm";
+
+const Register = () => {
+  const register = (registerData) => {
+    alert(JSON.stringify(registerData));
+  };
   return (
-    <div>
-      <h1>Hello Register</h1>
-    </div>
+    <>
+      <div className="bwm-form mt-5">
+        <div className="row">
+          <div className="col-md-5 mx-auto">
+            <h1 className="page-title">Register</h1>
+            <RegisterForm onSubmit={register} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
-export default register;
+export default Register;
