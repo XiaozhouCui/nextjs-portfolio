7a6e51f6e350b127618a4ed3730eebfc9d65a74c
Joe Cui <xiaozhou.cui@gmail.com>
2020-12-04 21:45:08 +1000

    Frontend: Updated login form

---
+++ components/forms/LoginForm.jsx
@@ -0,0 +1,37 @@
+import { useForm } from "react-hook-form";
+
+const LoginForm = ({ onSubmit }) => {
+  // handleSubmit will take in all form data and pass them onto "onSubmit" props as args
+  // register is the named export, DO NOT change to "login"
+  const { handleSubmit, register } = useForm();
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input
+          ref={register}
+          name="email"
+          type="email"
+          className="form-control"
+          id="email"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          ref={register}
+          name="password"
+          type="password"
+          className="form-control"
+          id="password"
+        />
+      </div>
+      <button type="submit" className="btn btn-main bg-blue py-2 ttu">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
