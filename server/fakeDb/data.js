0e7523d792097d502a63dbf928575200d41c4ef1
Joe Cui <xiaozhou.cui@gmail.com>
2020-12-09 20:57:39 +1000

    Backend: loaded dev data for ForumCategory and Topic

--- server/fakeDb/data.js
+++ server/fakeDb/data.js
@@ -3,6 +3,12 @@ const mongoose = require("mongoose");
 const user1Id = mongoose.Types.ObjectId();
 const user2Id = mongoose.Types.ObjectId();
 
+const forum1Id = mongoose.Types.ObjectId();
+const forum2Id = mongoose.Types.ObjectId();
+const forum3Id = mongoose.Types.ObjectId();
+
+const topic1Id = mongoose.Types.ObjectId();
+
 const data = {
   users: [
     {
@@ -59,6 +65,53 @@ const data = {
       description: "So much responsibility....Overloaaaaaad",
       startDate: "01/01/2010",
       endDate: "01/01/2011",
+      user: user1Id,
+    },
+  ],
+  forumCategories: [
+    {
+      _id: forum1Id,
+      title: "General Discussion",
+      subTitle: "Open any topic you want",
+      slug: "general-discussion",
+    },
+    {
+      _id: forum2Id,
+      title: "Job Requests",
+      subTitle: "Post here job opportunities",
+      slug: "job-requests",
+    },
+    {
+      _id: forum3Id,
+      title: "Developer Jokes",
+      subTitle: "Just funny developing stuff",
+      slug: "developer-jokes",
+    },
+  ],
+  topics: [
+    {
+      _id: topic1Id,
+      title: "How to learn JS",
+      slug: "how-to-learn-js",
+      content:
+        "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+      forumCategory: forum1Id,
+      user: user1Id,
+    },
+    {
+      title: "How to learn JAVA",
+      slug: "how-to-learn-java",
+      content:
+        "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+      forumCategory: forum1Id,
+      user: user1Id,
+    },
+    {
+      title: "How to learn C++",
+      slug: "how-to-learn-c++",
+      content:
+        "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+      forumCategory: forum1Id,
       user: user1Id,
     },
   ],
--- server/fakeDb/fakeDb.js
+++ server/fakeDb/fakeDb.js
@@ -1,17 +1,23 @@
-const { portfolios, users } = require("./data");
+const { portfolios, users, forumCategories, topics } = require("./data");
 
 const Portfolio = require("../database/models/portfolio");
 const User = require("../database/models/user");
+const ForumCategory = require("../database/models/forumCategory");
+const Topic = require("../database/models/topic");
 
 class FakeDb {
   async clean() {
-    await User.deleteMany({});
-    await Portfolio.deleteMany({});
+    // await User.deleteMany({});
+    // await Portfolio.deleteMany({});
+    await ForumCategory.deleteMany({});
+    await Topic.deleteMany({});
   }
 
   async addData() {
-    await User.create(users);
-    await Portfolio.create(portfolios);
+    // await User.create(users);
+    // await Portfolio.create(portfolios);
+    await ForumCategory.create(forumCategories);
+    await Topic.create(topics);
   }
 
   async populate() {
