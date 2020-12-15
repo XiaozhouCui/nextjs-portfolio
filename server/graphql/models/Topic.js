5a2c7718366c4ae7c712589a15cc0093e8d235eb
Joe Cui <xiaozhou.cui@gmail.com>
2020-12-15 14:19:23 +1000

    Backend: applied unique slug to avoid duplicate mongo error

--- server/graphql/models/Topic.js
+++ server/graphql/models/Topic.js
@@ -1,4 +1,6 @@
 const slugify = require("slugify");
+const uniqueSlug = require("unique-slug");
+
 class Topic {
   constructor(model, user) {
     this.Model = model;
@@ -7,6 +9,14 @@ class Topic {
 
   getAllByCategory(forumCategory) {
     return this.Model.find({ forumCategory })
+      .populate("user")
+      .populate("forumCategory");
+  }
+
+  async _create(data) {
+    // calling mongoose model to create data in DB
+    const createdTopic = await this.Model.create(data);
+    return this.Model.findById(createdTopic._id)
       .populate("user")
       .populate("forumCategory");
   }
@@ -26,11 +36,19 @@ class Topic {
       locale: "vi", // language code of the locale to use
     });
 
-    // calling mongoose model to create data in DB
-    const createdTopic = await this.Model.create(topicData);
-    return this.Model.findById(createdTopic._id)
-      .populate("user")
-      .populate("forumCategory");
+    let topic;
+
+    try {
+      topic = await this._create(topicData);
+      return topic;
+    } catch (e) {
+      if (e.code === 11000 && e.keyPattern && e.keyPattern.slug) {
+        topicData.slug += `-${uniqueSlug()}`;
+        topic = await this._create(topicData);
+        return topic;
+      }
+      return null;
+    }
   }
 }
 
