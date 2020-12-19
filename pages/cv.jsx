744129ecc8721506bc4ab685af1978e5cf0605cd
Joe Cui <xiaozhou.cui@gmail.com>
2020-12-19 16:57:12 +1000

    Frontend: added CV pdf file as iframe

--- pages/cv.jsx
+++ pages/cv.jsx
@@ -1,8 +1,18 @@
+import BaseLayout from "@/layouts/BaseLayout";
+
 const CV = () => {
   return (
-    <div>
-      <h1>My CV</h1>
-    </div>
+    <BaseLayout>
+      <div className="row mt-4">
+        <div className="col-md-8 offset-md-2">
+          <iframe
+            style={{ width: "100%", minHeight: "800px" }}
+            src="/Joe_CV.pdf"
+            frameBorder="0"
+          ></iframe>
+        </div>
+      </div>
+    </BaseLayout>
   );
 };
 
